557ab496a7e5dd132096dd55f5de78e862f0419c
Oliver Hansen <oliv045r@edu.easj.dk>
2020-12-03 17:46:45 +0100

    Første ændringer på index
    Header, landing og menu og arbejdet på de skrå linjer

---
+++ scripts.js
@@ -0,0 +1,8 @@
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100vw";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }  
