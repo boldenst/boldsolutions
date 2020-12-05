bbb124d3614f0ac5fd90528e76df2980dce7035c
Oliver Hansen <oliv045r@edu.easj.dk>
2020-12-05 01:20:06 +0100

    forhåbentlig virkende kontaktformular

---
+++ contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "kontakt@boldenst.dk";
+    $headers = "From: ".$mailFrom;
+    $txt = "Du har modtaget en mail fra ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
